34a13ffdfe7661ebdf98c77b56b265bdcc40ac69
gr00ze <vincenzop23@gmail.com>
2023-05-21 23:23:16 +0200

    Sounds

--- src/main/java/com/jojoskeleton/entity/JojoSkeleton.java
+++ src/main/java/com/jojoskeleton/entity/JojoSkeleton.java
@@ -7,7 +7,6 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.level.Level;
 
